2d250393795b850d1c6fe1db07152c64a1e8d808
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-06-16 20:55:28 +0545

    Initial Commit

---
+++ mySwiftApp/mySwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySwiftAppApp.swift
+//  mySwiftApp
+//
+//  Created by Saujan Bindukar on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct mySwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
